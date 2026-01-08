0439476ff812d735dc31eb0d1ae492d61c59ec6b
Harman <harmanjss10@gmail.com>
2026-01-08 10:27:58 +0530

    Add files via upload

---
+++ java/DataTypes.java
@@ -0,0 +1,22 @@
+/** 
+WAP
+1) declare variable of int, flaot, double. long, Boolean type.
+2)
+*/
+
+class DataTypes
+{
+   public static void main(String a[])
+{
+   int i1 = 20;
+   long l1 = 9798798876L;
+   float f = 3.4f;
+   double d = 33.5;
+   boolean b = true; 
+   System.out.println("int: " +i1);
+   System.out.println("long: " +l1); 
+   System.out.println("float: " +f);
+   System.out.println("double: " +d);
+   System.out.println("Boolean: " +b);
+  }
+  }
