33a99edc526452a5f5dafb385bbb8abacd981ca7
Harman <harmanjss10@gmail.com>
2026-01-13 13:30:36 +0530

    Update FirstPrg.java

--- Unit 1/FirstPrg.java
+++ Unit 1/FirstPrg.java
@@ -1,1 +1,19 @@
 
+public class FirstPrg
+{
+    public static void main(String args[])
+{
+   System.out.println("\uafff");
+   System.out.println("\124");
+   int binary = 0b010;
+   int octal = 07;
+   int hexa = 0xf;
+   System.out.println("Binary " + binary);
+   System.out.println("Octal " + octal);
+   System.out.println("Hexa " + hexa);
+   int i = 1_12_678;
+   System.out.println("d " + i);
+   var i1 = 10;
+    System.out.println("i1 " + i1);
+}
+}
