972fc7d31e5a7b290a5e7b517126b84ad9391b4e
Harman <harmanjss10@gmail.com>
2026-01-27 13:30:11 +0530

    Create HierarchicalDemo.java

---
+++ Unit2/HierarchicalDemo.java
@@ -0,0 +1,11 @@
+package unit2;
+public class HierarchicalDemo
+  {
+    public static void main(String[] args)
+    {
+      Triangle t = new Triangle();
+      t.setHeight(10);
+      t.setLength(12);
+      System.out.println("Area = " -t.cal_area());
+    }
+  }
