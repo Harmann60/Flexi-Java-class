a807a90ff58d089913cbfe71e93d676fda1cd11c
Harman <harmanjss10@gmail.com>
2026-01-27 13:40:36 +0530

    Add Box and ShipmentCost classes

---
+++ Unit2/Box.java
@@ -0,0 +1,14 @@
+class Box
+  {
+    Box();
+    private double length;
+    private double height;
+    private double weidth;
+  }
+
+class ShipmentCost extends Box
+  {
+    ShipmentCost();
+    private double cost;
+  }
+    
