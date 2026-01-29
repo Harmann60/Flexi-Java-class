c425c2a04a338924e86e65a51508ea8570fa1637
Harman <harmanjss10@gmail.com>
2026-01-29 08:58:15 +0530

    Update Box class with constructors and variable fixes
    Refactor Box class to include multiple constructors and fix variable declarations.

--- Unit2/Box.java
+++ Unit2/Box.java
@@ -1,12 +1,42 @@
-class Box
+public class Box
   {
-    Box();
     private double length;
     private double height;
-    private double weidth;
+    private double weidth;th
+    Box()
+    {
+      this.height =-1;
+      this.length=-1;
+      this.width=-1;
   }
-
-class ShipmentCost extends Box
+    Box(double length,double height,double width)
+    {
+      this.height=height;
+        this.length=length;
+      this.width=width;
+    }
+    Box(Box b)
+    {
+      this.width=b.width;
+      this.height=b.height;
+      this.length=b.length;
+    }
+    Box(double side)
+    {
+      this.height=side;
+      this.length=side;
+      this.width=side;
+    }
+    public double getWidth()
+    {
+    }
+    public double getHeight()
+    {
+    }
+    public double getLength()
+    {
+    }
+    class ShipmentCost extends Box
   {
     ShipmentCost();
     private double cost;
