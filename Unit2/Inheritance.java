e2bdec9e016a55ad1c92cd192517a6e40c0fd644
Harman <harmanjss10@gmail.com>
2026-01-29 09:28:41 +0530

    Add new ShipmentCost instance and output statements
    Added new ShipmentCost instance and print statements.

--- Unit2/Inheritance.java
+++ Unit2/Inheritance.java
@@ -6,4 +6,7 @@ public class Inheritance
       ShipmentCost c1=new ShipmentCost();
       ShipmentCost c2=new ShipmentCost(1,2,3,4);
       ShipmentCost c3=new ShipmentCost(c2);
+      ShipmentCost c4=new ShipmentCost();
+      System.out.println("Volume is :" +c2.volume());
+      Systemout.println("Total Shipping Cost is :" +c2.cal,cShipCost());
     }
