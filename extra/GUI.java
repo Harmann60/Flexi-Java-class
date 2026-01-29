052c739cac9f563b41b57fba29e44a5c6ded7947
Harman <harmanjss10@gmail.com>
2026-01-29 09:55:51 +0530

    Add username and password labels to GUI
    Added labels for username and password fields in GUI.

--- extra/GUI.java
+++ extra/GUI.java
@@ -4,6 +4,10 @@ public class GUI extends JFrame
 {
   GUI(String title)
   {
+    JLabel uname = new JLabel("User  Name");
+    JLabel password = new JLabel("Passowrd");
+    add(uname);
+    add(password);
     super(title);
     setSize(400,200);
     setVisible(true);
