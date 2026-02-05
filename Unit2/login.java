3c095a65cbab04e3a75ae9ff7b383ae25feda139
Harman <harmanjss10@gmail.com>
2026-02-05 10:06:47 +0530

    Refactor login class and improve formatting

--- Unit2/login.java
+++ Unit2/login.java
@@ -3,13 +3,12 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class login extends JFrame implements ActionListener
-{
+public class login extends JFrame implements ActionListener {
     JLabel uname, upass;
-    JTextField tname,tpass;
-    JButton submit , reset;
-    login(String title)
-    {
+    JTextField tname, tpass;
+    JButton submit, reset;
+
+    login(String title) {
         super(title);
         uname = new JLabel("Username");
         upass = new JLabel("Password");
@@ -24,21 +23,29 @@ public class login extends JFrame implements ActionListener
         add(tpass);
         add(submit);
         add(reset);
-        submit.addActionListener(this) ;
+        submit.addActionListener(this);
         reset.addActionListener(this);
-        setLocation(100,100);
-        setSize(500,500);
+        setLocation(100, 100);
+        setSize(500, 500);
         setVisible(true);
     }
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         new login("Login Verification");
     }
 
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
-        tname.setText("");
-        tpass.setText("");
-
+        if (actionEvent.getSource() == reset) {
+            tname.setText("");
+            tpass.setText("");
+        }
+        if (actionEvent.getSource() == submit) {
+            if (tname.getText().equals("67") && tpass.getText().equals("69")) {
+                JOptionPane.showMessageDialog(this,"Succesfully Enter unlike you cant enter in her heart","Message",JOptionPane.INFORMATION_MESSAGE);
+            } else {
+                JOptionPane.showMessageDialog(this,"Forget Passowrd? Like she forgot you","Message",JOptionPane.ERROR_MESSAGE);
+            }
+        }
     }
 }
