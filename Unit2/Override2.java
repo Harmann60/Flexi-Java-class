76d041c111f129c7421ed592d7576b8ee08b8239
Harman <harmanjss10@gmail.com>
2026-02-10 13:24:36 +0530

    Create Override2.java

---
+++ Unit2/Override2.java
@@ -0,0 +1,17 @@
+package Class;
+public class in1 extends spur
+{
+    int twice(int num);
+    {
+        return num*2;
+    }
+}
+public static void main(String[] args)
+{
+    spur ord = new spur();
+    System.out.println(ord.twice(3));
+    in1 ord1 = new in1();
+    System.out.println(int1.twice(3));
+    ord = ord1;
+    System.out.println(ord.twice(3));
+}
