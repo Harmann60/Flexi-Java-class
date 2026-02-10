1aa2fa176eaa0d9b36ca217fe490de1340bfae98
Harman <harmanjss10@gmail.com>
2026-02-10 13:40:34 +0530

    Add Packd class with a default constructor

---
+++ Unit2/Packd.java
@@ -0,0 +1,15 @@
+package packd;
+
+public class Packd
+{
+    int num1;
+    Packd()
+    {
+        num1=10;
+    }
+    public static void main(String[] args)
+    {
+        Packd pac = new Packd();
+        System.out.println(pac.num1);
+    }
+}
