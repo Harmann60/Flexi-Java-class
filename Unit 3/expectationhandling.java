078fd959c9a49fbb82512ab0eed79cb8bda6d0c4
Harman <harmanjss10@gmail.com>
2026-02-24 12:52:46 +0530

    Add expectationhandling class with main method

---
+++ Unit 3/expectationhandling.java
@@ -0,0 +1,10 @@
+public class expectationhandling
+{
+    public static void main(String[] args) throws InterruptedException {
+        for(int i=1;i<=5;i++)
+        {
+            System.out.println(i);
+            Thread.sleep(2000);
+        }
+    }
+}
